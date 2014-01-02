2283766b5386b6bbaaa3eaf3c730ca258423c4b7
kyle <xlorkhanx@gmail.com>
2014-01-02 09:40:15 -0800

    moving some stuff into closure

--- property-sort.js
+++ property-sort.js
@@ -1,7 +1,9 @@
-var naturalSort = require("javascript-natural-sort"),
-    deep = require("deep-access");
+
 
 module.exports = function (path) {
+    var naturalSort = require("javascript-natural-sort"),
+        deep = require("deep-access");
+
     return function (a, b) {
         return naturalSort(deep(a, path), deep(b, path));
     };
