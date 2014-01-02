dc67a62108bf8539d4d5f2277953dc061e70ebf0
kyle <xlorkhanx@gmail.com>
2014-01-02 09:57:49 -0800

    making it case insensitive

--- property-sort.js
+++ property-sort.js
@@ -4,6 +4,8 @@ module.exports = function (path) {
     var naturalSort = require("javascript-natural-sort"),
         deep = require("deep-access");
 
+    naturalSort.insensitive = true;
+
     return function (a, b) {
         return naturalSort(deep(a, path), deep(b, path));
     };
