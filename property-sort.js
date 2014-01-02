6b5e43be067894108aab639fc07c9c28f9567156
kyle <xlorkhanx@gmail.com>
2014-01-02 09:30:10 -0800

    fixing an error I made with deep

--- property-sort.js
+++ property-sort.js
@@ -2,8 +2,6 @@ var naturalSort = require("javascript-natural-sort"),
     deep = require("deep-access");
 
 module.exports = function (path) {
-    path = path.split(".");
-
     return function (a, b) {
         return naturalSort(deep(a, path), deep(b, path));
     };
